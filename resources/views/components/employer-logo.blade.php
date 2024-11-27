4bbf91db95f540309ef190a5d55aef70699955bc
Ez IT Solutions <chrishultberg@ez-it-solutions.com>
2024-11-27 13:29:46 -0800

    Update employer-logo.blade.php
    picsum.photos uses a new api for their temp images - updated api call for temp images.

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -1,3 +1,7 @@
 @props(['employer', 'width' => 90])
 
-<img src="{{ asset($employer->logo) }}" alt="" class="rounded-xl" width="{{ $width }}">
+<!-- This will be for production -->
+<!-- <img src="{{ asset($employer->logo) }}" alt="" class="rounded-xl" width="{{ $width }}"> -->
+
+<!-- This generates temporary logo images -->
+<img src="http://picsum.photos/seed/{{ rand(0, 100000) }}/{{ $width }}/{{ $width }}" alt="" class="rounded-xl" width="{{ $width }}">
