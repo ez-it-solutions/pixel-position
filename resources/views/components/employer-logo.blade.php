fe9fdd99012da0f52b36f8f5c09b04c6780c82c1
Ez IT Solutions <chrishultberg@ez-it-solutions.com>
2024-11-27 13:36:29 -0800

    Update employer-logo.blade.php
    Using new "random" api from picsum.photos

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -4,4 +4,14 @@
 <!-- <img src="{{ asset($employer->logo) }}" alt="" class="rounded-xl" width="{{ $width }}"> -->
 
 <!-- This generates temporary logo images -->
-<img src="http://picsum.photos/seed/{{ rand(0, 100000) }}/{{ $width }}/{{ $width }}" alt="" class="rounded-xl" width="{{ $width }}">
+<!-- <img src="http://picsum.photos/seed/{{ rand(0, 100000) }}/{{ $width }}/{{ $width }}" alt="" class="rounded-xl" width="{{ $width }}"> -->
+
+
+<!-- <img src="http://picsum.photos/seed/{{ rand(0, 100000) }}" alt="" class="rounded-xl" width="{{ $width }}"> -->
+<!-- <img src="{{ asset($employer->logo) }}" alt="" class="rounded-xl" width="{{ $width }}"> -->
+
+<!-- <img src="http://picsum.photos/{{ $width }}" alt="" class="rounded-xl" width="{{ $width }}"> -->
+
+<!-- <img src="http://picsum.photos/seed/{{ rand(0, 100000) }}/{{ $width }}/{{ $width }}" alt="" class="rounded-xl" width="{{ $width }}"> -->
+
+<img src="http://picsum.photos/{{ $width }}/{{ $width }}?ramdom={{ rand(0, 100000) }}" alt="" class="rounded-xl" width="{{ $width }}">
